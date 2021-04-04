9aed236d97a404adcb2bf90d4e703ce7514712d0
OTAHARUNA <radwimps.love867@gmail.com>
2021-04-04 10:53:31 +0900

    最終的な成果だよ

--- src/Likebutton.jsx
+++ src/Likebutton.jsx
@@ -3,6 +3,8 @@ import React, {useState,useEffect } from 'react';
 const Likebutton = () => {
   //counterの値によって変えられるようになる
   const [count, counter] = useState(0);
+  //②空の配列を第二引数に入れると2回目からのイベントリスナーが実行されない為以下追加
+  const [limit, release] = useState(true);
   const countUp = () => {
     counter(count + 1)
   }
@@ -10,14 +12,22 @@ const Likebutton = () => {
     console.log('UnMounting');
     document.getElementById("counter").addEventListener('click', countUp)
     //ここまでMounting 以下、UpdateMounting/UnMounting
-    return () => (
+    if (count == 10) {
+      counter(0);
+    }
+    return () => {
       document.getElementById("counter").removeEventListener('click', countUp)
-    )
-
-
-  });
+    }
+  }, [true]);
+  //②第2引数に引数に空の配列を入れると初めの1回（初めのマウント時にしかイベントリスナーが実行されない）のみ実行される　一度クリックすると次のrenderが実行されreturn内のクリーンアップされる為次のイベントリスナーが実行されない
+  //第2引数の値が変わるとコールバック関数の値が変わる
   return (
-    <button id="counter">いいね数： {count}</button>
+    //②limit解除する為のボタン作成が必要になってくる
+    <>
+      <button id="counter">いいね数： {count}</button>
+      <button onClick={() => release(!limit) }>もっといいねしたい</button>
+    </>
+    //上記でlimitがtrueからfalseに変わる
   )
 }
 export default Likebutton
